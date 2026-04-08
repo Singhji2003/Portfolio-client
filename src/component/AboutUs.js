1184a64c9a415fa418db5441bcab4c74ecb6598a
Arpan <arpansinghrajput123@gmail.com>
2026-04-08 09:05:48 +0530

    updatd

--- src/component/AboutUs.js
+++ src/component/AboutUs.js
@@ -1,19 +1,28 @@
-const AboutUs = ()=>{
-    return(
-        <>
-         <div id="main"></div>
-    <section id="about" className="about-me">
-        <h2 className="about-heading">About <span>Me</span></h2>
+const AboutUs = () => {
+  return (
+    <>
+      <div id="main"></div>
+      <section id="about" className="about-me">
+        <h2 className="about-heading">
+          About <span>Me</span>
+        </h2>
         <div className="profile-image">
-            <img src={require("./Image/arpanimg.jpeg")} alt=""/>
-            <span className="circle"></span>
+          <img src={require("./Image/arpanimg.jpeg")} alt="" />
+          <span className="circle"></span>
         </div>
         <div className="about-content">
-            <h3>Full Stack Developer !</h3>
-            <p>Hii.. There I am Arpan Singh. I am from Bihar but for study Purpose I live in Gurugram Haryana. I am Pursuing Bvoc Software Development from Ramanujan collge Delhi University. And I am in My third year. I learn many things through online and also imply it in real life using Projects. I did many projects on My personal basis. Which shows my skills.  </p>
+          <h3>Full Stack Developer !</h3>
+          <p>
+          I’m a Full Stack Web Developer at Infiheal, building modern,
+          user-focused web solutions using ReactJS, AngularJS, and Tailwind CSS.
+          Alongside my full-time role, I’ve completed multiple freelancing
+          projects in WordPress and the MERN stack, strengthening both my
+          frontend and backend expertise. I enjoy solving real-world problems
+          and continuously improving my skills in fast-paced development
+          environments. 🚀</p>
         </div>
-    </section>
-        </>
-    )
-}
+      </section>
+    </>
+  );
+};
 export default AboutUs;
